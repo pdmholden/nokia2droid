e9719007a71f4996f07dc09ebcd6cd000171d85f
Paul Holden <pdmholden@gmail.com>
2015-08-11 12:55:43 -0700

    Write the VCF to a file with the same name

--- nokia2droid.rb
+++ nokia2droid.rb
@@ -8,15 +8,18 @@ require 'vcardigan'
 def forward_convert options
   src = File.open(options.filename)
   doc = Nokogiri::XML(src)
-  dst = VCardigan.create(:version => '4.0')
+  vcf = VCardigan.create(:version => '3.0')
 
   unless doc.at_xpath("//c:Notes").nil?
-    dst.note doc.at_xpath("//c:Notes").children.to_s
+    vcf.note doc.at_xpath("//c:Notes").children.to_s
   end
-  dst.tel doc.at_xpath("//c:Number").children.to_s, :type => 'CELL'
-  dst.name doc.at_xpath("//c:FamilyName").children.to_s,
+  vcf.tel doc.at_xpath("//c:Number").children.to_s, :type => 'CELL'
+  vcf.name doc.at_xpath("//c:FamilyName").children.to_s,
     doc.at_xpath("//c:GivenName").children.to_s
-  dst.fullname doc.at_xpath("//c:GivenName").children.to_s + doc.at_xpath("//c:FamilyName").children.to_s
+  # FN listed in surname-family name order, accoring to Asian tradition
+  # no space, assumes name in Chinese characters
+  vcf.fullname doc.at_xpath("//c:FamilyName").children.to_s +
+    doc.at_xpath("//c:GivenName").children.to_s
   # FN mandatory for vcard?
 
   if options.dump
@@ -25,7 +28,12 @@ def forward_convert options
     print "Number: ", doc.at_xpath("//c:Number").children.to_s, "\n\n"
   end
 
-  # puts dst
+  # puts vcf
+  out_filename = File.dirname(options.filename) + File::SEPARATOR +
+    File.basename(options.filename, ".*") + ".vcf"
+  File.open(out_filename, "w") do |dst|
+    dst.write(vcf)
+  end
   src.close
 end
 
@@ -34,7 +42,7 @@ OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
 
   opts.on("-f", "--file NAME", "File to convert") do |f|
-    options.filename = f
+    options.filename = File.absolute_path(f)
   end
   opts.on("-d", "--dump", "Show the contact data on stdout") do |x|
     options.dump = x
