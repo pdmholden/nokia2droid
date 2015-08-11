12665652e64def885ef7c549844df5ac526f5561
Paul Holden <pdmholden@gmail.com>
2015-08-10 20:45:14 -0700

    Dump out desired values from Nokia/MS .contact

--- nokia2droid.rb
+++ nokia2droid.rb
@@ -2,14 +2,24 @@
 
 require 'optparse'
 require 'ostruct'
+require 'nokogiri'
+
+def forward_convert filename
+    src = File.open(filename)
+    doc = Nokogiri::XML(src)
+    # p doc.at_xpath("//c:Notes").children.to_s
+    p doc.at_xpath("//c:Number").children.to_s
+    p doc.at_xpath("//c:FamilyName").children.to_s
+    p doc.at_xpath("//c:GivenName").children.to_s
+end
 
 options = OpenStruct.new
 OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
 
   opts.on("-f", "--file NAME", "File to convert") do |f|
-    options.file = f
+    options.filename = f
   end
 end.parse!
 
-p options.file
+forward_convert options.filename
