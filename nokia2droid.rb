ea670e8ab36120f38a847e2dd8fc047c6c4a9161
Paul Holden <pdmholden@gmail.com>
2015-08-11 12:12:58 -0700

    Add option to dump contact information to stdout

--- nokia2droid.rb
+++ nokia2droid.rb
@@ -5,8 +5,8 @@ require 'ostruct'
 require 'nokogiri'
 require 'vcardigan'
 
-def forward_convert in_filename
-  src = File.open(in_filename)
+def forward_convert options
+  src = File.open(options.filename)
   doc = Nokogiri::XML(src)
   dst = VCardigan.create(:version => '4.0')
 
@@ -19,6 +19,13 @@ def forward_convert in_filename
   dst.fullname doc.at_xpath("//c:GivenName").children.to_s + doc.at_xpath("//c:FamilyName").children.to_s
   # FN mandatory for vcard?
 
+  if options.dump
+    print "Surname: ", doc.at_xpath("//c:FamilyName").children.to_s, "\n"
+    print "Name: ", doc.at_xpath("//c:GivenName").children.to_s, "\n"
+    print "Number: ", doc.at_xpath("//c:Number").children.to_s, "\n\n"
+  end
+
+  # puts dst
   src.close
 end
 
@@ -29,6 +36,9 @@ OptionParser.new do |opts|
   opts.on("-f", "--file NAME", "File to convert") do |f|
     options.filename = f
   end
+  opts.on("-d", "--dump", "Show the contact data on stdout") do |x|
+    options.dump = x
+  end
 end.parse!
 
-forward_convert options.filename
+forward_convert options
